a8d4b3c08813d16faab01e1a4977d70ccfecbeb9
yongminKwon1 <ym.kwon@lipis.kr>
2025-01-02 15:08:07 +0700

    initialize

---
+++ src/test/java/danang03/STBackend/StBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package danang03.STBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
