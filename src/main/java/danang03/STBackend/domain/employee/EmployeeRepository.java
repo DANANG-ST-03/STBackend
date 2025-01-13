58faf86e4b96ed7fe60423d41b6a9d8330b1ca72
hjjo <123654393+hjjo0@users.noreply.github.com>
2025-01-13 16:41:36 +0700

    feat: add text suggestion by name

--- src/main/java/danang03/STBackend/domain/employee/EmployeeRepository.java
+++ src/main/java/danang03/STBackend/domain/employee/EmployeeRepository.java
@@ -1,10 +1,17 @@
 package danang03.STBackend.domain.employee;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     boolean existsByEmail(String email);
 
+    @Query("SELECT e.name FROM Employee e WHERE e.name LIKE %:input%")
+    List<String> findNamesStartingWith(@Param("input") String input, Pageable limit);
 }
--- src/main/java/danang03/STBackend/domain/projects/ProjectRepository.java
+++ src/main/java/danang03/STBackend/domain/projects/ProjectRepository.java
@@ -1,8 +1,15 @@
 package danang03.STBackend.domain.projects;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
+    @Query("SELECT p.name FROM Project p WHERE p.name LIKE %:input%")
+    List<String> findNamesStartingWith(@Param("input") String input, Pageable pageable);
 }
