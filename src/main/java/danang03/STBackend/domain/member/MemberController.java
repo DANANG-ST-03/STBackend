e6a709fe74e05fd5c3a01c68d4e744a33b94cc02
yongminKwon1 <ym.kwon@lipis.kr>
2025-01-16 16:48:14 +0700

    fix: get admins by page

--- src/main/java/danang03/STBackend/domain/member/MemberController.java
+++ src/main/java/danang03/STBackend/domain/member/MemberController.java
@@ -1,39 +1,39 @@
 package danang03.STBackend.domain.member;
 
-import danang03.STBackend.domain.employee.dto.EmployeeResponse;
+
 import danang03.STBackend.domain.member.dto.MemberResponse;
 import danang03.STBackend.dto.GlobalResponse;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller("admin")
+@RestController
+@RequestMapping("/admin")
+@RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
-    @Autowired
-    public MemberController(MemberService memberService) {
-        this.memberService = memberService;
-    }
+
 
     @GetMapping
-    public ResponseEntity<GlobalResponse> getEmployeesByPage(
+    public ResponseEntity<GlobalResponse> getMembers(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size,
             Pageable pageable) {
         Page<MemberResponse> members = memberService.getMembersByPage(PageRequest.of(page, size));
         GlobalResponse globalResponse = GlobalResponse.builder()
                 .status(200)
-                .message("get Admins success")
+                .message("get admins success")
                 .data(members).build();
         return ResponseEntity.ok(globalResponse);
     }
 
-//    @PatchMapping("")
+//    @PatchMapping("/password")
+//    public ResponseEntity<GlobalResponse> changePassword()
 }
